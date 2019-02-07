49e651731bf7638292beb9e2e2157af23531e3bc
Ihor Paliy <darkfree97@gmail.com>
2019-02-08 00:03:57 +0200

    CORS settings for suppurting Angular

--- picture_server/settings.py
+++ picture_server/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # External apps
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
 
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,6 +124,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
 }
+
+# CORS_ORIGIN_WHITELIST = [
+#     'localhost:4200',
+#     '127.0.0.1:4200',
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
